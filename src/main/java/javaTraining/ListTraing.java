98ae92e4fee580c837657ae8add335772b1e3759
KOby <kobyd100@gmail.com>
2023-03-15 21:19:38 +0200

    use selenium base

--- src/main/java/javaTraining/ListTraing.java
+++ src/main/java/javaTraining/ListTraing.java
@@ -8,10 +8,12 @@ public class ListTraing {
 
     public static void main(String[] args) {
         List<String> names = Arrays.asList("Elya", "Jorge", "Moran", "David", "Daniela", "Omar");
+        Boolean isPass = true;
+        Boolean isFalse = false;
+
         for (String name : names) {
             if (name.contains("or")) {
                 System.out.println(name);
-
             }
 
             if (name.equals("Omar")) {
@@ -20,6 +22,23 @@ public class ListTraing {
             if (name.equalsIgnoreCase("omar")) {
                 System.out.println("Omar Found");
 
+            }
+
+            if (isPass){
+                System.out.println("into condition True ");
+            }
+
+            if (isPass==true){
+                System.out.println("into condition True ");
+            }
+
+            if  (!isFalse){
+                System.out.println("into condition False ");
+
+            }
+
+            if  (isFalse==false){
+                System.out.println("into condition False ");
 
             }
         }
