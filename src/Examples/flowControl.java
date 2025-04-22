a7a5622c29ee45052d3b8da579989247fb56fa38
dkdk1 <dkdk100@walla.co.il>
2025-04-22 10:28:11 +0300

    first commit for 0ct 2024

--- src/Examples/flowControl.java
+++ src/Examples/flowControl.java
@@ -3,6 +3,7 @@ package Examples;
 public class flowControl {
     public static void main(String[] args) {
 
+
         for (int i = 0; i < 5; i++) {
             System.out.println(i);
             if (i == 2){
